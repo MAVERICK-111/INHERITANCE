4cd6a91571e76fbebc78460c239d722a151409c0
harshogale04 <harshogale04@gmail.com>
2025-01-21 23:21:41 +0530

    Solved mongo issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
@@ -10,5 +10,41 @@ const connectDB = async () => {
     }
 };
 
+module.exports = connectDB;*/
+
+
+
+const uri = process.env.MONGO_URI;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Function to connect to MongoDB
+async function connectDB() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
 module.exports = connectDB;
+
+///* MONGO_URI=mongodb://localhost:27017/AMA
+//JWT_SECRET=your_jwt_secret
+//PORT=5000
+//AUTH0_DOMAIN=dev-ucsp4ge1ss5vocyz.us.auth0.com
+//AUTH0_AUDIENCE=https://dev-ucsp4ge1ss5vocyz.us.auth0.com/api/v2/ 
 
