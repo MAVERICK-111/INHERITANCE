ca1c3a79e1c18569e127a15be65fa44eb6639034
MAVERICK <piyush3patil2005@gmail.com>
2025-01-29 03:47:34 +0530

    logout button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,20 @@
 // src/components/Header.js
 import React from 'react';
 import './Header.css'; 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';  // Import the useAuth0 hook
 
-
+const LogoutButton = () => {
+  const { logout } = useAuth0();  // Access the logout function from useAuth0
+  
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
 
 function Header() {
- 
   return (
     <header>
       <div className="Top_section">
@@ -14,8 +22,8 @@ function Header() {
           <Link to="/homepage">Logo</Link></div>
         <div className="Username">
           <Link to="/profile">Profile</Link>
+          <LogoutButton />
         </div>
-        
       </div>
     </header>
   );
