01937d470827060ee76e39c58aeb06168276232d
MAVERICK <piyush3patil2005@gmail.com>
2025-01-29 19:01:01 +0530

    backend cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import Header from './components/Header';
 import Landingpage from './pages/LandingPage';
-import LoginPage from './pages/Login';
 import Homepage from './pages/Homepage';
-import Home from './pages/Home';
 import AMA from './pages/AMA';
 import Hobbies from './pages/Hobbies';
 import Profile from './pages/Profile';
@@ -13,7 +11,6 @@ import Alumni from './pages/Alumni'; // Import Alumni page
 //import Request from './pages/Request';
 import Messages from './pages/Messages';
 import Noticeboard from './pages/Noticeboard';
-import HobbyChat from './components/HobbyChat';
 import './App.css';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -39,10 +36,8 @@ function App() {
         <Routes>
           {/* Public Route */}
           <Route path="/" element={<Landingpage />} />
-          <Route path="/login" element={<LoginPage />} />  
 
           {/* Protected Routes */}
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="/homepage" element={<ProtectedRoute><Homepage /></ProtectedRoute>} />
           <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
 
@@ -52,7 +47,6 @@ function App() {
           {/*<Route path="/Request" element={<ProtectedRoute><Request /></ProtectedRoute>} />*/}
           <Route path="/Messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
           <Route path="/Noticeboard" element={<ProtectedRoute><Noticeboard /></ProtectedRoute>} />
-          <Route path="/hobby-chat" element={<ProtectedRoute><HobbyChat /></ProtectedRoute>} />
           
         </Routes>
       </div>
